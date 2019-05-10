aca1afd2404fb5b154d99b2ebb5cff5f1b9d91d3
Ezekiel Mangalu <emangalu@gmail.com>
2019-05-10 11:15:47 +0800

    Removed serve and used webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path')
 
 module.exports = {
+  devServer: {
+    contentBase: [
+      path.join(__dirname, 'public'),
+      path.join(__dirname, 'dist')
+    ],
+    hot: true
+  },
   module: {
     rules: [
       {
