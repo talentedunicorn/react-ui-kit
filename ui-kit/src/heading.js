584a6634fb325b260d5d9dd27540b24dcc5d665f
Ezekiel Mangalu <emangalu@gmail.com>
2019-05-13 12:11:38 +0800

    Added props to heading for underline, installed classnames

--- ui-kit/src/heading.js
+++ ui-kit/src/heading.js
@@ -1,21 +1,37 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import PropTypes from "prop-types"
+import cx from "classnames"
 
 import HeadingStyles from "./heading.module.css"
 
 const Heading = props => {
   switch(props.level) {
     case 1: 
-      return <h1 className={HeadingStyles.H1}>{props.text}</h1>
+      return <h1 className={cx(
+        HeadingStyles.H1,
+        {[HeadingStyles.underlined]: props.underlined}
+      )}>{props.text}</h1>
     case 2:
-      return <h2 className={HeadingStyles.H2}>{props.text}</h2>
+      return <h2 className={cx(
+        HeadingStyles.H2,
+        {[HeadingStyles.underlined]: props.underlined}
+      )}>{props.text}</h2>
     case 3:
-      return <h3 className={HeadingStyles.H3}>{props.text}</h3>
+      return <h3 className={cx(
+        HeadingStyles.H3,
+        {[HeadingStyles.underlined]: props.underlined}
+      )}>{props.text}</h3>
     case 4:
-      return <h4 className={HeadingStyles.H4}>{props.text}</h4>
+      return <h4 className={cx(
+        HeadingStyles.H4,
+        {[HeadingStyles.underlined]: props.underlined}
+      )}>{props.text}</h4>
     case 5:
-      return <h5 className={HeadingStyles.H5}>{props.text}</h5>
+      return <h5 className={cx(
+        HeadingStyles.H5,
+        {[HeadingStyles.underlined]: props.underlined}
+      )}>{props.text}</h5>
     default:
       return null
   }
@@ -23,7 +39,8 @@ const Heading = props => {
 
 Heading.propTypes = {
   level: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  underlined: PropTypes.bool
 }
 
 export default Heading
