a73fdb894ff7b4d6dcd0ce1d9ba6d6c85f858cf9
Ezekiel Mangalu <emangalu@gmail.com>
2019-05-13 12:23:18 +0800

    added styles for buttons

--- ui-kit/src/button.js
+++ ui-kit/src/button.js
@@ -4,10 +4,29 @@ import ButtonStyles from "./button.module.css"
 
 import PropTypes from "prop-types"
 
-const Button = props => 
-  <button className={ButtonStyles.Button} onClick={() => props.handleClick()}>{ props.children }</button>
+const buttonTypes = ['hollow', 'error']
+
+const Button = props =>  {
+
+  let style = ButtonStyles.default
+
+  switch(props.type) {
+    case 'hollow':
+      style = ButtonStyles.hollow
+      break
+    default:
+      break
+  }
+
+  return <button 
+    className={style} 
+    disabled={props.disabled}
+    onClick={() => props.handleClick()}>{ props.children }</button>
+}
 
 Button.propTypes = {
-  handleClick: PropTypes.func
+  type: PropTypes.oneOf(buttonTypes),
+  handleClick: PropTypes.func,
+  disabled: PropTypes.bool
 }
 export default Button
